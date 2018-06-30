34cd81ecfcdc6f9891e951931643d7a11194f2ac
petrarch1603 <petrarch1603@gmail.com>
2018-06-30 09:48:11 -0600

    get buttons to work

--- calculator.js
+++ calculator.js
@@ -38,18 +38,79 @@ function onClickNumber(e) {
 function onClickOperator(e) {
   let oper = e.target.textContent;
   display.operator = oper;
+  calculator.push(display.number);
   display.number = '';
   display.update();
+  calculator.push(oper);
+  console.log(oper);
 }
 
 function onClickEquals(e) {
-  console.log('equal pushed');
+  console.log("equal pushed");
+  calculator.push(display.number);
+  display.number = calculator.evaluate();
+  display.operator = '';
+  display.update();
 }
 
 function onClickClear(e) {
   console.log('clear pushed');
+  calculator.operand_stack = [];
+  calculator.operator_stack = [];
+  display.number = 0;
+  display.operator = '';
+  display.update();
 }
 
 function onClickDecimal(e) {
   console.log('. pushed');
+}
+
+let calculator = {
+  operand_stack: [],
+  operator_stack: [],
+
+  evalTop: function() {
+    let b = Number(this.operand_stack.pop());
+    let a = Number(this.operand_stack.pop());
+    let oper = this.operator_stack.pop();
+    let result = 0;
+    switch(oper) {
+      case '+':
+        result = a + b;
+        break;
+      case '-':
+        result = a - b;
+        break;
+      case '*':
+        result = a * b;
+        break;
+      case '/':
+        if (b === 0) {
+          result = 'ERROR - Cannot Divide by Zero!'
+        }
+        result = a / b;
+        break;
+    }
+    if (result = 'ERROR - Cannot Divide by Zero!') {
+      this.operator_stack = [];
+    }
+    this.operand_stack.push(result);
+  },
+
+  push: function(value) {
+    let n = Number(value);
+    if (!isNaN(n)) {
+      this.operand_stack.push(value);
+    } else if(value.match(/[*+-\/]/g)){
+      this.operator_stack.push(value);
+    }
+  },
+
+  evaluate: function() {
+    while (this.operator_stack.length > 0) {
+      this.evalTop();
+    }
+    return this.operand_stack[0];
+  },
 }
